b414dc5d3f30fb9ec89e0847e12f7ca2b17138ed
Akshudhana <akshathad998@gmail.com>
2024-12-01 14:41:19 +0530

    2ndcommit

--- src/test/java/com/ApiWizProject/CreatingApiLinting.java
+++ src/test/java/com/ApiWizProject/CreatingApiLinting.java
@@ -11,10 +11,13 @@ public class CreatingApiLinting extends BaseClass{
 
 	@Test
 	public void creatingLinting() throws InterruptedException {
+		//Rule set name
 		String name="qa-testing";
+		
+		//RuleSet discription
 		String rulesetdiscription="qa-testing";
 		
-		//Creating ApiLintingPage object and sending the values
+		//Creating ApiLintingPage object and sending the values..
 		ApiLintingPage apilintingg=new ApiLintingPage(driver);
 		apilintingg.ApiLintingCreate(driver,name, rulesetdiscription, "info-tittle", "Ensure all the swaggers has tittle tags", "$.info.tittle");
 	}
